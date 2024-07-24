6b5141c5c62b49daa1f44808305baee852c5733f
saswatsahoo07 <sahoosaswat07@gmail.com>
2024-07-24 11:57:16 +0530

    Add files via upload

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Emp(models.Model):
+    name=models.CharField(max_length=50)
+    pno=models.CharField(max_length=50)
+    email=models.CharField(max_length=50)
+    un=models.CharField(max_length=50)
+    pw=models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
